2bdf655211852b3bb466fac88192f00760b62459
Muhammad Fadli Syukur <muhammadfadlisyukur16@gmail.com>
2021-04-27 12:53:28 +0700

    menambahkan fitu  profil

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tugas5_monitoring/theme.dart';
+import 'package:tugas5_monitoring/widgets/profile_tile.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.h),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(10.h, 10.h, 10.h, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.menu, color: primaryColor),
+                    Text(
+                      'Profile',
+                      style: TextStyle(
+                        color: primaryColor,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.notifications, color: primaryColor)
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Image.asset('assets/lead.png', height: 150.h),
+              SizedBox(height: 20.h),
+              Text("Muhammad Fadli Syukur", style: titleTextStyle),
+              SizedBox(height: 5.h),
+              Text("20180801305 | Manager"),
+              SizedBox(height: 20.h),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20.h),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25.r),
+                ),
+                child: Column(
+                  children: [
+                    ProfileTile(
+                      icon: Icons.notifications,
+                      title: 'Notifikasi',
+                      color: Colors.red,
+                    ),
+                    SizedBox(height: 15.h),
+                    ProfileTile(
+                      icon: Icons.person_pin,
+                      title: 'Edit Profil',
+                      color: Colors.blue[300],
+                    ),
+                    SizedBox(height: 15.h),
+                    ProfileTile(
+                      icon: Icons.date_range,
+                      title: 'Absensi',
+                      color: Colors.purple[900],
+                    ),
+                    SizedBox(height: 15.h),
+                    ProfileTile(
+                      icon: Icons.badge,
+                      title: 'Pekerjaan',
+                      color: Colors.amber[700],
+                    ),
+                    SizedBox(height: 15.h),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20.h),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25.r),
+                ),
+                child: Column(
+                  children: [
+                    ProfileTile(
+                      icon: Icons.settings,
+                      title: 'Pengaturan',
+                      color: Colors.black,
+                    ),
+                    SizedBox(height: 15.h),
+                    ProfileTile(
+                      icon: Icons.help,
+                      title: 'Bantuan',
+                      color: Colors.green[300],
+                    ),
+                    SizedBox(height: 15.h),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
